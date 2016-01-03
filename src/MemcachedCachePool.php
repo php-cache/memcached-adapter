1f7e0fc9d8e7287a5580f6f1f48a90794ac2ef5a
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-03 02:45:26 +0100

    Init

---
+++ src/MemcachedCachePool.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of php-cache\doctrine-adapter package.
+ *
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cache\Adapter\Memcached;
+
+use Cache\Adapter\Common\AbstractCachePool;
+use Cache\Adapter\Common\CacheItem;
+use Cache\Adapter\Common\Exception\InvalidArgumentException;
+use Cache\Taggable\TaggableItemInterface;
+use Cache\Taggable\TaggablePoolInterface;
+use Cache\Taggable\TaggablePoolTrait;
+use Predis\Client;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * @author Aaron Scherer <aequasi@gmail.com>
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class MemcachedCachePool extends AbstractCachePool
+{
+    /**
+     * @var \Memcached
+     */
+    private $cache;
+
+    /**
+     * @param \Memcached $cache
+     */
+    public function __construct(\Memcached $cache)
+    {
+        $this->cache = $cache;
+    }
+
+
+    protected function fetchObjectFromCache($key)
+    {
+        return $this->cache->get($key);
+    }
+
+    protected function clearAllObjectsFromCache()
+    {
+        return $this->cache->flush();
+    }
+
+    protected function clearOneObjectFromCache($key)
+    {
+        return $this->cache->delete($key);
+    }
+
+    protected function storeItemInCache($key, CacheItemInterface $item, $ttl)
+    {
+        if ($ttl === null) {
+            $ttl = 0;
+        }
+
+        return $this->cache->set($key, $item, $ttl);
+    }
+}
